b33241fd5cf1eeb5214e822f0c5bdaffe0eba7ff
Iamneo <techadmin@iamneo.ai>
2023-08-01 14:05:32 +0000

    Backup Commit

--- angularapp/src/app/component/header/header.component.spec.ts
+++ angularapp/src/app/component/header/header.component.spec.ts
@@ -1,3 +1,23 @@
-<header class="header-section" id="headerContainer">
-    <h3>Calory Tracker</h3>
-</header>
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HeaderComponent } from './header.component';
+
+describe('HeaderComponent', () => {
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HeaderComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(HeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
