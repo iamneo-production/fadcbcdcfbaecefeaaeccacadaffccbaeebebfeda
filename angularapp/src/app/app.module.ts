337cef5600c085a544e265792e5556645d51e91a
Iamneo <techadmin@iamneo.ai>
2023-08-01 13:41:39 +0000

    Backup Commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FoodListComponent } from './food-list.component';
-import { FoodSearchPipe } from './food-search.pipe';
-import { MyFoodsComponent } from './my-foods/my-foods.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { NewFoodComponent } from './component/new-food/new-food.component';
+import { TodayComponent } from './today/today/today.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FoodListComponent,
-    FoodSearchPipe,
-    MyFoodsComponent
+    HeaderComponent,
+    FooterComponent,
+    NewFoodComponent,
+    TodayComponent,
   ],
-  imports: [BrowserModule, FormsModule, HttpModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
